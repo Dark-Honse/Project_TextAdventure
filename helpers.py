a962b3ed3cee4fb3d861181eb133e7761b04c23a
Jamie Clynes <jamietclynes@hotmail.co.uk>
2025-05-22 18:25:16 +0100

    bridged file communication so that helpers and areas are imported. the info (values) still need passing in the call so that helpers can see what it needs

--- helpers.py
+++ helpers.py
@@ -1,8 +1,6 @@
 
 
-def player_view(location: str, areas: dict) -> None:
+def view(location, areas):
     """print the player's view."""
 
-    print("\n" + areas[location]["description"])
-
-player_view("larger room", areas)
+    print(areas[location]["description"])
--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+from helpers import view
+from areas import areas
+
 playing = False
 
+player_location = ""
+
+print("Welcome! Ready to play?")
+cmd = input("> ").strip().lower()
+
+if cmd in ["yes", "y", "yep"]:
+    playing = True
+    player_location = "stone room"
+else:
+    print("Maybe next time. Goodbye!")
+
+while playing:
+    print("You wake up in a dark room.")
+    cmd = input("> ").strip().lower()
+    if cmd in ["look", "look around", "view"]:
+        view(player_location, areas)
